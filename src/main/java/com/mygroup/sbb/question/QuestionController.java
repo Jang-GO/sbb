8134ec0e2c33b47a003ee8203df4c3da68f23409
Jang Hyeon Seo <jang8195@naver.com>
2023-07-31 13:22:50 +0900

    modify question_list numbering

--- src/main/java/com/mygroup/sbb/question/QuestionController.java
+++ src/main/java/com/mygroup/sbb/question/QuestionController.java
@@ -1,7 +1,5 @@
 package com.mygroup.sbb.question;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
