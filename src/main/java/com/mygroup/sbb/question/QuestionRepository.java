e5bc6a3677c44885de623c896ba1519e2ad9c038
Jang Hyeon Seo <jang8195@naver.com>
2023-07-31 11:55:04 +0900

    Apply pagenation

--- src/main/java/com/mygroup/sbb/question/QuestionRepository.java
+++ src/main/java/com/mygroup/sbb/question/QuestionRepository.java
@@ -1,5 +1,7 @@
 package com.mygroup.sbb.question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
@@ -8,4 +10,5 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     List<Question> findBySubjectLike(String subject);
     Question findBySubjectAndContent(String subject, String content);
     Question findBySubject(String subject);
+    Page<Question> findAll(Pageable pageable);
 }
